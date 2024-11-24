1ee2082c90e925c75358eac725ce4760c609b97a
lancedalanon <lanceorville5@gmail.com>
2024-11-24 21:31:09 +0800

    Fix issue of not building

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false,
+          path: false,
+        };
+      }
+      return config;
+    },
+};
 
 export default nextConfig;
